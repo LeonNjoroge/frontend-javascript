53a5dd67f45e09e0f015a1bc53cfb0b1b93058b2
LeonNjoroge <leonmuraguri@gmail.com>
2025-05-19 15:35:46 +0300

    Feat: Complete Prj 0 Tsk 2

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,8 +1,12 @@
- interface Teacher{
+export interface Teacher{
     readonly firstName: string;
     readonly lastName: string;
     fullTimeEmployer: boolean;
     yearsOfExperience?: boolean;
     location: string;
     [key: string]: any;
+}
+
+export interface Directors extends Teacher{
+    numberOfReports: number;
 }
